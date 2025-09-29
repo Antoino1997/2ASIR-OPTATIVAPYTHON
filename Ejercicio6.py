cb53c7ed3c5b395f706b7dd49e262a469dfb7a3d
Antonio1997 <antoniolepe14@gmail.com>
2025-09-29 13:37:14 +0200

    add

---
+++ Ejercicio6.py
@@ -0,0 +1,7 @@
+"""Tupla con los datos de una persona, que se desempaqueta y se imprime con el formato establecido."""
+persona = ("Ana", 25 , "Madrid")
+nombre, edad, ciudad = persona
+
+print("Nombre:", nombre)
+print("Edad:", edad)
+print("Ciudad:", ciudad)
