736237e282b86e7837933e1a41a1480d432a4a27
Antonio Pérez Galán <49763952+Antoino1997@users.noreply.github.com>
2025-12-02 11:33:26 +0100

    Delete Ejercicio9_1.py

--- Ejercicio9_1.py
+++
@@ -1,6 +0,0 @@
-nombres = ["Ana", "Luis", "Marta"]
-notas_matematicas = [8, 7, 9]
-notas_fisica = [9, 6, 10]
-
-for a,  b, c in zip(nombres, notas_matematicas, notas_fisica):
-    print(a, "-", "Matemáticas:", b,"Física:", c)
