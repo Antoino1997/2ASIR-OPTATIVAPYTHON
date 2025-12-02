95aa059e663ed31958013407667849caacfac874
Antonio Pérez Galán <49763952+Antoino1997@users.noreply.github.com>
2025-12-02 11:32:36 +0100

    Delete Ejercicio2.py

--- Ejercicio2.py
+++
@@ -1,8 +0,0 @@
-def registrar_usuario(nombre, edad, ciudad="Madrid"):
-    """Muestra los datos básicos del usuario."""
-    print(f"Usuario: {nombre}, Edad: {edad}, Ciudad: {ciudad}")
-
-# Llamadas válidas
-registrar_usuario("Antonio", 28, "Huelva") #Posicionales
-registrar_usuario("Antonio", 28) #Ciudad omitida
-registrar_usuario(ciudad="Huelva", nombre="Antonio", edad=28) #Argumentos desordenados
