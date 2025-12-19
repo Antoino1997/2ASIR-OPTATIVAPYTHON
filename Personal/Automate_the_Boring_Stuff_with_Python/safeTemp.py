c73c2e9a0843137e7f8b58cd00b0d9aa45e19ccf
Antonio Pérez Galán <49763952+Antoino1997@users.noreply.github.com>
2025-12-19 09:15:19 +0100

    Add files via upload

---
+++ Personal/Automate_the_Boring_Stuff_with_Python/safeTemp.py
@@ -0,0 +1,14 @@
+print('Enter C or F to indicate Celsius or Fahrenheit:')
+scale = input()
+print('Enter the number of degrees:')
+degrees = int(input())
+if scale == 'C':
+    if 16 <= degrees <= 38:
+        print('Safe')
+    else:
+        print('Dangerous')
+elif scale == 'F':
+    if 60.8 <= degrees <= 100.4:
+        print('Safe')
+    else:
+        print('Dangerous')
