46ade0d3f6ebfd00eae962d58089242d61cfbb35
Antonio Pérez Galán <49763952+Antoino1997@users.noreply.github.com>
2025-12-19 08:55:01 +0100

    Add files via upload

---
+++ Personal/Automate_the_Boring_Stuff_with_Python/perimeterAreaCalculator.py
@@ -0,0 +1,10 @@
+width = int(input("Enter the width for the rectangle: "))
+length = int(input("Enter the length for the rectangle: "))
+
+area = width * length
+perimeter = width * 2 + length * 2
+
+print("Area of the rectangle:")
+print(area)
+print("Perimeter of the rectangle:")
+print(perimeter)
